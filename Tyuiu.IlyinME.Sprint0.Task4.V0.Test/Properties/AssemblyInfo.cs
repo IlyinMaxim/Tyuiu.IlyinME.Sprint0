b6340a7692b4e3aaed5fe3fdd46b2624482c6fec
Tyuiu.IlyinME@outlook.com <maxchance05@mail.ru>
2023-09-13 20:45:28 +0500

    Task4

---
+++ Tyuiu.IlyinME.Sprint0.Task4.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.IlyinME.Sprint0.Task4.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.IlyinME.Sprint0.Task4.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4447cb38-a425-40e9-a1cf-d1d40663c0dc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.IlyinME.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.IlyinME.Sprint0.Task4.V0.Lib;
+
+namespace Tyuiu.IlyinME.Sprint0.Task4.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Addition(11, 7));
+            Console.WriteLine(DataService.Subtraction(11, 7));
+            Console.WriteLine(DataService.Multiplication(7, 11));
+            Console.WriteLine(DataService.Division(77, 11));
+
+            Console.ReadKey();
+        }
+    }
+}
