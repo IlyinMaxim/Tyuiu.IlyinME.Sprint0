dcc110bd0f01c1ff4ec146b271cab43685d00fe1
Tyuiu.IlyinME@outlook.com <maxchance05@mail.ru>
2023-09-14 20:43:46 +0500

    Task4

--- Tyuiu.IlyinME.Sprint0.Task4.V0.Test/DataServiceTest.cs
+++ Tyuiu.IlyinME.Sprint0.Task4.V0.Test/DataServiceTest.cs
@@ -29,7 +29,7 @@ namespace Tyuiu.IlyinME.Sprint0.Task4.V0.Test
         [TestMethod]
         public void CheckedDivisionValid()
         {
-            Assert.AreEqual(5, DataService.Division(10, 2));
+            Assert.AreEqual(8, DataService.Division(16, 2));
         }
     }
 }
